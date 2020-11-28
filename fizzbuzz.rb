e03a337edd31d8ea0e7cf8804b8ca7df720ad199
Danny Brandt <dvbrandt90@gmail.com>
2020-11-28 06:42:35 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,10 +4,5 @@ def fizzbuzz(number)
   if number % 3 == 0
     "Fizz"
   end
-  elsif number % 5 == 0 
-    "Buzz"
-  end
-  elsif number % 3 == 0  && number % 5 == 0 
-    "FizzBuzz"
-  end
+ 
 end
